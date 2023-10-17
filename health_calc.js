7fc2e50f861786255c73086ad11658e4aeeb99b8
Rick <23171338+rickjfp@users.noreply.github.com>
2023-10-16 21:51:09 -0400

    Add FitPoints Calc
    Add HR Zone and fitpoints calc functionality.

--- health_calc.js
+++ health_calc.js
@@ -52,3 +52,59 @@ function bmi_calc() {
     // Display BMI category
     document.getElementById("category").innerHTML = "Category: " + category;
 }
+
+
+
+// Exercise Time Tracker
+function hr_zone_calc() {
+
+    // Set zone variables
+    var age = parseFloat(document.getElementById("age").value);
+    
+    // Check if the inputs are valid numbers
+    if (isNaN(age) || age <= 0 || age > 110) {
+        alert("Please enter valid age.");
+        return;
+    }
+
+    var maxHeartRate = 211 - (0.64 * age);
+    
+    document.getElementById("z1_hr").innerHTML = 
+        "Zone 1 (50%-60%): " + Math.round(maxHeartRate * 0.5) + "-" + Math.round(maxHeartRate * 0.6) + " bpm";
+    document.getElementById("z2_hr").innerHTML = 
+        "Zone 2 (60%-70%): " + Math.round(maxHeartRate * 0.6) + "-" + Math.round(maxHeartRate * 0.7) + " bpm";
+    document.getElementById("z3_hr").innerHTML = 
+        "Zone 3 (70%-80%): " + Math.round(maxHeartRate * 0.7) + "-" + Math.round(maxHeartRate * 0.8) + " bpm";
+    document.getElementById("z4_hr").innerHTML = 
+        "Zone 4 (80%-90%): " + Math.round(maxHeartRate * 0.8) + "-" + Math.round(maxHeartRate * 0.9) + " bpm";
+    document.getElementById("z5_hr").innerHTML = 
+        "Zone 5 (90%-100%): " + Math.round(maxHeartRate * 0.9) + "-" + Math.round(maxHeartRate * 1) + " bpm";
+}
+
+function fit_points_calc() {
+    
+    var z1m = parseFloat(document.getElementById("z1m").value);
+    var z2m = parseFloat(document.getElementById("z2m").value);
+    var z3m = parseFloat(document.getElementById("z3m").value);
+    var z4m = parseFloat(document.getElementById("z4m").value);
+    var z5m = parseFloat(document.getElementById("z5m").value);
+
+    // Check if the inputs are valid numbers
+    if ( isNaN(z1m) || z1m < 0 
+        || isNaN(z2m) || z2m < 0
+        || isNaN(z3m) || z3m < 0 
+        || isNaN(z4m) || z4m < 0 
+        || isNaN(z5m) || z5m < 0 ) {
+        alert("Please enter valid minutes.");
+        return;
+    }
+
+    // fit points
+    var ftpts_wk_target = 150;
+    var ftpts = (z1m * 0.5) + (z2m * 1) + (z3m * 1) + (z4m * 1.5) + (z5m * 2);
+
+    document.getElementById("fitpts").innerHTML = 
+        "Weekly Target: " + ftpts_wk_target;
+    document.getElementById("fitpts").innerHTML = 
+        "Fitness Points: " + ftpts;
+}
